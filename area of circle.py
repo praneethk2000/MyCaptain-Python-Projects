c36e4ba819ecfa55def9dcf666176acb6f0b51d9
praneethk2000 <66408609+praneethk2000@users.noreply.github.com>
2020-08-02 16:53:48 +0530

    Added area of circle.py
    Contains code for first task of first project

---
+++ area of circle.py
@@ -0,0 +1,8 @@
+# Calculating the area of a cirxle with user inputting the radius
+
+r = float(input("Enter the radius of the circle: "))
+pi = 3.14
+
+a = pi*r*r
+
+print ("The area of the circle with radius",r, "is:",a) 
